03401e289909b3ae95c2897ea8d0270da5798b40
Aino K <onja@MacBook-Pro.local>
2024-12-03 15:21:41 +0200

    Day 2 finally working

--- 2_reactor/script2.py
+++ 2_reactor/script2.py
@@ -1,47 +1,46 @@
-# each report can make one mistake in the logic
-# once an error is found, remove the item that caused the error and run the check again
-from itertools import dropwhile
-
 def read_input():
     # read input file into separate lists, convert strings to int
-    with open("test", "r") as input:
+    with open("input", "r") as input:
         reports = [i.split() for i in input]
         return [[int(i) for i in r] for r in reports]
 
+def check_ascending(r, check):
+        if all(r[i] < r[i+1] and not r[i] <= r[i+1]-4 for i in range(0,len(r)-1)):
+            return True
+        elif (check):
+            safe_with_mods = False
+            for i in range(0, len(r)):
+               new_r = r.copy()
+               del new_r[i]
+               safe_with_mods += check_ascending(new_r, False)
+            if safe_with_mods > 0:
+                return True
+            else:
+                return False
+        else:
+            return False
 
-def check_ascending(r, original_length, recheck):
-    new_list = []
-    for i in range(0,len(r)-1):
-        if r[i] < r[i+1] and r[i]+3 >= r[i+1]:
-            new_list.append(r[i])
-    new_list.append(r[-1])
-    if(len(new_list) == original_length-1) and recheck==True:
-        return check_ascending(new_list, original_length, False)
-    elif(len(new_list) < original_length-1):
-        return 0
-    else:
-        return 1
-
-def check_descending(r, original_length, recheck):
-    new_list = []
-    for i in range(0,len(r)-1):
-        if r[i] > r[i+1] and r[i]-3 <= r[i+1]:
-            new_list.append(r[i])
-    new_list.append(r[-1])
-    if(len(new_list) == original_length-1) and recheck==True:
-        return check_descending(new_list, original_length, False)
-    elif(len(new_list) < original_length-1):
-        return 0
-    else:
-        return 1
-
-
+def check_descending(r, check):
+        if all(r[i] > r[i+1] and not r[i] >= r[i+1]+4 for i in range(0,len(r)-1)):
+            return True
+        elif (check):
+            safe_with_mods = False
+            for i in range(0, len(r)):
+               new_r = r.copy()
+               del new_r[i]
+               safe_with_mods += check_descending(new_r, False)
+            if safe_with_mods > 0:
+                return True
+            else:
+                return False
+        else:
+            return False
 
 def check_safe(reports):
     safe = 0
     for r in reports:
-        safe += check_ascending(r, len(r), True)
-        safe += check_descending(r, len(r), True)
+        safe += check_ascending(r, True)
+        safe += check_descending(r, True)
     print(safe)
 
 def main():
