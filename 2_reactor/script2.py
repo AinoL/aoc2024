c36eb5aa8871dc2e2af4a305b32f90ccd16c3ae9
Aino K <onja@MacBook-Pro.local>
2024-12-03 15:29:42 +0200

    Added some comments for lols

--- 2_reactor/script2.py
+++ 2_reactor/script2.py
@@ -5,21 +5,32 @@ def read_input():
         return [[int(i) for i in r] for r in reports]
 
 def check_ascending(r, check):
+        # Check if everything in report is ascending normally
         if all(r[i] < r[i+1] and not r[i] <= r[i+1]-4 for i in range(0,len(r)-1)):
+            # All good, return true
             return True
         elif (check):
+            # Assume nothing works
             safe_with_mods = False
+            # Loop through all variations of once modified report
             for i in range(0, len(r)):
+               # Make a copy for every possibility to not modify the original list
                new_r = r.copy()
                del new_r[i]
+               # Recurse the original loop, change check boolean to false to not go here again
+               # If any of the modifications worked, add that to a counter
                safe_with_mods += check_ascending(new_r, False)
             if safe_with_mods > 0:
+                # A working modification found, return true
                 return True
             else:
+                # Nothing worked, return false
                 return False
         else:
+            # Nothing worked and boolean won't allow check anymore because this is second round, return false
             return False
 
+# Do everything again for descending order
 def check_descending(r, check):
         if all(r[i] > r[i+1] and not r[i] >= r[i+1]+4 for i in range(0,len(r)-1)):
             return True
@@ -38,9 +49,11 @@ def check_descending(r, check):
 
 def check_safe(reports):
     safe = 0
+    # Go through all reports for ascending and descending
     for r in reports:
         safe += check_ascending(r, True)
         safe += check_descending(r, True)
+    # How many safe reports were found
     print(safe)
 
 def main():
